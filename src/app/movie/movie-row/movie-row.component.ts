46bb2a5e7069e7e481d7337b1e195ca46c09ae3b
Pedro Lemos <pedroemlemos@gmail.com>
2020-05-23 23:34:10 -0300

    Movie row component

---
+++ src/app/movie/movie-row/movie-row.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+import { MovieModel } from '../model/movie.model';
+
+@Component({
+  selector: 'app-movie-row',
+  templateUrl: 'movie-row.component.html',
+  styleUrls: ['./movie-row.component.css'],
+})
+export class MovieRowComponent {
+
+  @Input()
+  public movies: MovieModel[];
+
+  constructor(private readonly sanitizer: DomSanitizer) { }
+
+  get movieGenre(): string {
+    return this.movies.find(movie => !!movie.genre).genre;
+  }
+
+  public getMovieImageUrl(movie: MovieModel): SafeResourceUrl {
+    const url = movie.videoUrl.replace('www', 'img').replace('embed', 'vi') + '/0.jpg';
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -1,16 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ConfigModule } from '../config/config.module';
+import { MovieRowComponent } from './movie-row/movie-row.component';
 import { MovieRestService } from './service/movie-rest.service';
 import { MovieService } from './service/movie.service';
 
 @NgModule({
+  declarations: [
+    MovieRowComponent,
+  ],
   imports: [
+    CommonModule,
     ConfigModule,
   ],
   providers: [
     MovieService,
     MovieRestService,
   ],
+  exports: [
+    MovieRowComponent,
+  ]
 })
 export class MovieModule { }
