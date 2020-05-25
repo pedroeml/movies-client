2c753715e2f34f5b09f2c1e791e7ef0ca108568b
Pedro Lemos <pedroemlemos@gmail.com>
2020-05-24 23:51:57 -0300

    Dashboard component updates

--- src/app/metrics/dashboard/dashboard.component.ts
+++ src/app/metrics/dashboard/dashboard.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
-import { UserModel } from '../../user/model/user.model';
-import { GenreViews } from '../model/genre-views.interface';
 import { RegionMovieCollection } from '../model/region-movie-collection.interface';
-import { RegionMovieModel } from '../model/region-movie.model';
 import { MetricsService } from '../service/metrics.service';
 
 @Component({
@@ -12,58 +9,15 @@ import { MetricsService } from '../service/metrics.service';
   styleUrls: ['dashboard.component.css'],
 })
 export class DashboardComponent {
-  private readonly limit: number;
-  private readonly user: UserModel;
-  private country: string;
-  public countries: string[];
   public moviesByRegion: RegionMovieCollection;
-  public topUsers: UserModel[];
-  public topMovies: RegionMovieModel[];
-  public topGenres: GenreViews[];
 
   constructor(private readonly service: MetricsService) {
-    this.limit = 3;
-    this.user = this.service.getLoggedUser();
-    this.loadTopUsers();
     this.loadMoviesByRegion();
-  }
-
-  get selectedCountry(): string {
-    return this.country;
-  }
-
-  set selectedCountry(country: string) {
-    if (this.country !== country) {
-      this.country = country;
-      this.updateTopMovies();
-    }
-  }
-
-  private updateTopMovies(): void {
-    this.topMovies = this.service.topMoviesByRegion(this.moviesByRegion, this.country, this.limit + 2);
-  }
-
-  public totalViewsByCountry(movie: RegionMovieModel): number {
-    return movie.getViewsByRegion(this.selectedCountry);
-  }
-
-  private loadTopUsers(): void {
-    this.service.getTopUsers(this.limit).subscribe(
-      users => { this.topUsers = users; }
-    );
   }
 
   private loadMoviesByRegion(): void {
     this.service.getMoviesByRegion().subscribe(
-      collection => {
-        this.countries = this.service.getAvailableRegions(collection);
-        this.topGenres = this.service.topGenres(collection);
-        this.moviesByRegion = collection;
-      },
-      () => { },
-      () => {
-        this.selectedCountry = this.user.country;
-      }
+      collection => { this.moviesByRegion = collection; }
     );
   }
 }
--- src/app/metrics/metrics.module.ts
+++ src/app/metrics/metrics.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,13 +12,20 @@ import { UserModule } from '../user/user.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MetricsRoutingModule } from './metrics-routing.module';
 import { MetricsService } from './service/metrics.service';
+import { TopGenresComponent } from './top-genres/top-genres.component';
+import { TopMoviesComponent } from './top-movies/top-movies.component';
+import { TopUsersComponent } from './top-users/top-users.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    TopGenresComponent,
+    TopMoviesComponent,
+    TopUsersComponent,
   ],
   imports: [
     CommonModule,
+    MatCardModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
     MatSelectModule,
